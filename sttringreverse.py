cc09942db1bae7515f9892e33334c08ec6217b59
nancymewada <nen.mewada@gmail.com>
2017-02-26 17:16:26 +0530

    Add files via upload

---
+++ sttringreverse.py
@@ -0,0 +1,12 @@
+def Stringreverse(st):
+	if len(st)<2:
+		return st
+	return reverse(st[1:]) +st[0]
+
+print Stringreverse('nancy') #1 way not good as recursion uses more space
+
+
+print 'nancy'[::-1] #2 way
+
+st = 'nancy'
+print ''.join([st[i] for i in range(len(st)-1,-1,-1)])#3 way
