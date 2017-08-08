df361693956c2f78a7323c0751fcf78455ab2144
nancymewada <nen.mewada@gmail.com>
2017-08-09 01:51:53 +0530

    Add files via upload

---
+++ spiral_ncrossm.py
@@ -0,0 +1,38 @@
+n = 3
+range_n = n-1
+d = (n*n) + 1
+
+a= [[i for i in range(i,i+n)] for i in range(1,n*n,n)]
+
+al =a 
+print a
+nd= n
+nc = n
+rangec_n = nc -1
+c = 0
+for i in range(0,(n/2)+1):
+	for ab in range(i,nd-i):
+		c += 1
+		if c < d:
+			print a[i][ab]
+		else:
+			break
+	for cd in range(i+1,nc-i):
+		c += 1
+		if c < d:
+			print a[cd][range_n-i]	
+		else:
+			break
+	for ef in range(range_n-i-1,i-1-i,-1):
+		c += 1
+		if c < d:
+			print a[rangec_n-i][ef]
+		else:
+			break
+	for gh in range(rangec_n-i-1,i,-1):
+		c += 1
+		if c < d:
+			print a[gh][i]
+		else:
+			break
+
